a4908afbe6a4918be4b3eaa08f828148d01de316
phouvong <pphouvong@gmail.com>
2024-10-20 13:12:31 +0700

    First upload

---
+++ lib/util/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const robotoRegular = TextStyle(
+  fontFamily: 'Lao',
+  fontWeight: FontWeight.w400,
+);
+
+const robotoMedium = TextStyle(
+  fontFamily: 'Lao',
+  fontWeight: FontWeight.w500,
+);
+
+const robotoBold = TextStyle(
+  fontFamily: 'Lao',
+  fontWeight: FontWeight.w700,
+);
+
+const robotoBlack = TextStyle(
+  fontFamily: 'Lao',
+  fontWeight: FontWeight.w900,
+);
